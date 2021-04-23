d4a1b8bf63116cf3361e1153ab7e27cb907f430d
oumarluck <oumarlucky@gmail.com>
2021-04-23 16:57:06 +0200

    modif mission

---
+++ database/migrations/2021_04_23_144642_create_mission_internes_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMissionInternesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mission_internes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->bigInteger('agent_id');
+            $table->bigInteger('lieumission_id');
+            $table->bigInteger('vehicule_id');
+            $table->bigInteger('sourcefinancement_id');
+            $table->string('objet');
+
+            $table->date('datedepart')->nullable();
+            $table->date('dateretour')->nullable();
+            $table->string('qrcode');
+            $table->boolean('active')->nullable()->default(true);
+            $table->boolean('incidencefinanciere')->nullable()->default(false);
+
+            $table->foreign('agent_id')->references('id')->on('agents')->onDelete('cascade');
+            $table->foreign('lieumission_id')->references('id')->on('lieu_missions')->onDelete('cascade');
+            $table->foreign('vehicule_id')->references('id')->on('vehicules')->onDelete('cascade');
+            $table->foreign('sourcefinancement_id')->references('id')->on('source_financements')->onDelete('cascade');
+
+            $table->string("created_by");
+            $table->string("update_by");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mission_internes');
+    }
+}
